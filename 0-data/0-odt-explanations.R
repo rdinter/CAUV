de85138cae1278d22fe59216dac60e05c3647a47
Robert Dinterman <robert.dinterman@gmail.com>
2024-03-08 23:21:04 +0000

    new version of ODT CAUV explainers

--- 0-data/0-odt-explanations.R
+++ 0-data/0-odt-explanations.R
@@ -1,6 +1,5 @@
 # Ohio Taxation statistics:
-# http://www.tax.ohio.gov/tax_analysis/tax_data_series/
-#  publications_tds_property.aspx
+# https://tax.ohio.gov/government/real-state/cauv
 
 # ---- start --------------------------------------------------------------
 
@@ -17,33 +16,74 @@ folder_create <- function(x, y = "") {
 
 # Create a directory for the data
 local_dir    <- folder_create("0-data/odt")
-data_source  <- folder_create("/raw", local_dir)
+explainers   <- folder_create("/explainers", local_dir)
 
-tax_site <- paste0("http://www.tax.ohio.gov/tax_analysis/tax_data_series/",
-                   "publications_tds_property.aspx")
+# Header to bypass tax.ohio forcing 403 errors
+moz_head <- c("user-agent" =
+                paste0("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) ",
+                       "Gecko/20100101 Firefox/73.0"))
 
-# ---- cauv-explainations -------------------------------------------------
 
-explainers   <- folder_create("/explainers", data_source)
+# ---- kinda-automated ----------------------------------------------------
 
-# Should have all the explainers downloaded for future use!
+cauv_site <- session("https://tax.ohio.gov/government/real-state/cauv",
+                     add_headers(moz_head))
 
-cauvexp <- read_html("https://www.tax.ohio.gov/real_property/cauv.aspx")
+cauv_href <- cauv_site |> 
+  html_elements("li:nth-child(2) :nth-child(1)") |> 
+  html_attr("href")  
 
-link1 <- cauvexp %>% 
-  html_nodes("a") %>% 
-  html_text()
+cauv_text <- cauv_site |> 
+  html_elements("li:nth-child(2) :nth-child(1)") |> 
+  html_text2()
 
-link2 <- cauvexp %>% 
-  html_nodes("a") %>% 
-  html_attr("href") %>% 
-  paste0("http://www.tax.ohio.gov", .)
+df <- tibble(
+  text = cauv_text,
+  href = cauv_href
+) |> 
+  filter(grepl("Explanation", text) & !is.na(href))
 
-cauvexp <- data_frame(text = link1, url = link2) %>% 
-  filter(grepl("explanation", tolower(text)))
-
-tax_download <- purrr::pmap(cauvexp, function(text, url){
-  Sys.sleep(sample(seq(0,1,0.25), 1))
-  dfile <- paste0(explainers, "/", tolower(basename(text)), ".pdf")
-  if (!file.exists(dfile)) download.file(url, dfile)
+pmap(df, function(text, href) {
+  file_name <- paste0(explainers, "/", text, ".pdf")
+  cauv_url  <- paste0("https://tax.ohio.gov", href)
+  
+  if (!file.exists(file_name)) download.file(cauv_url, file_name,
+                                             mode = "wb", headers = moz_head)
+  
 })
+
+# ---- brute-force --------------------------------------------------------
+
+
+# Need to update the links, see top of file
+base_url   <- "https://tax.ohio.gov/static/"
+
+expl_files <- c(paste0("real_property/cauv_2009_explanation_of_",
+                       "the_cauv_calculation.pdf"),
+                "real_property/cauv_memorandum_2010.pdf",
+                paste0("real_property/explanation%20of%202011%20cauv",
+                       "%20calculation%20-%20final.pdf"),
+                "personal_property/cauv%202012%20revised%20explanation.pdf",
+                "personal_property/explanation_of_2013_calculations.pdf",
+                "personal_property/explanation%202014.pdf",
+                "personal_property/explanation2015.pdf",
+                "personal_property/explanation2016.pdf",
+                "personal_property/explanation2017.pdf",
+                "personal_property/explanation2018.pdf",
+                "personal_property/explanation2019.pdf",
+                "real_property/2020explanationwithexhibits.pdf",
+                "real_property/2021explanationwithexhibits.pdf",
+                "real_property/2022Explanationwithexhibts.pdf",
+                "real_property/2023explanationwithexhibts.pdf")
+expl_urls <- paste0(base_url, expl_files)
+
+cauv_exp_files <- str_replace_all(basename(expl_files), "%20", " ")
+
+
+map2(expl_urls, cauv_exp_files, function(x, y) {
+  file_name <- paste0(explainers, "/", y)
+  if (!file.exists(file_name)) download.file(x, file_name, mode = "wb",
+                                             headers = moz_head)
+})
+
+
