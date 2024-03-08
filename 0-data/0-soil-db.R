bb9fd32e2aac2ead59105e57a8c2fce4cb18a06d
Robert Dinterman <robert.dinterman@gmail.com>
2024-03-08 23:22:12 +0000

    soil db ordered

--- 0-data/0-soil-db.R
+++ 0-data/0-soil-db.R
@@ -7,7 +7,6 @@
 
 # ---- start --------------------------------------------------------------
 
-
 library("tidyverse")
 library("soilDB")
 
@@ -26,7 +25,8 @@ q <- paste0("SELECT L.areaname, M.mukey, M.musym, M.muname, ",
             "FROM mapunit M ",
             "INNER JOIN legend L on L.lkey = M.lkey and",
             " L.areasymbol LIKE 'OH%' ")
-res <- SDA_query(q)
+res <- SDA_query(q) |> 
+  arrange(areaname, desc(mapunit_acres))
 glimpse(res)
 
 write_csv(res, paste0(local_dir, "/nrcs_county_soils.csv"))
@@ -40,14 +40,15 @@ write_rds(res, paste0(local_dir, "/nrcs_county_soils.rds"))
 
 # ---- with-pi ------------------------------------------------------------
 
-q2 <- "SELECT l.areaname, musym, muname, muacres, farmlndcl, 
+q2 <- "SELECT l.areaname, m.*, musym, muname, muacres, farmlndcl, 
         textsubcat AS PI
        FROM legend AS l
        INNER JOIN mapunit m ON m.lkey = l.lkey AND areasymbol LIKE 'OH%'
        LEFT JOIN mutext AS mt ON mt.mukey = m.mukey 
         AND mapunittextkind LIKE 'miscellaneous notes' AND textcat = 'PI'
        ORDER BY areasymbol, musym"
-res2 <- SDA_query(q2)
+res2 <- SDA_query(q2) |> 
+  arrange(areaname, desc(muacres))
 glimpse(res2)
 
 write_csv(res2, paste0(local_dir, "/nrcs_county_soils_wpi.csv"))
@@ -65,20 +66,30 @@ write_rds(res2, paste0(local_dir, "/nrcs_county_soils_wpi.rds"))
 
 # Alternate:
 
-q3 <- "SELECT l.areaname, musym, muname, muacres, farmlndcl, compname,
-        nirrcapcl, nirrcapscl, textsubcat AS PI
+q3 <- "SELECT l.areaname, musym, muname, muacres, farmlndcl, c.*, 
+        textsubcat AS PI, m.muacres * (c.comppct_r / 100.0) AS acres_estimate
        FROM legend AS l
        INNER JOIN mapunit m ON m.lkey = l.lkey AND areasymbol LIKE 'OH%'
-       INNER JOIN mutext AS mt ON mt.mukey = m.mukey 
+       LEFT JOIN mutext AS mt ON mt.mukey = m.mukey 
         AND mapunittextkind LIKE 'miscellaneous notes' AND textcat = 'PI'
-       INNER JOIN component  AS c ON c.mukey = m.mukey
-        AND majcompflag LIKE 'Yes'
+       LEFT JOIN component  AS c ON c.mukey = m.mukey
        ORDER BY areasymbol, musym"
-res3 <- SDA_query(q3)
+res3 <- SDA_query(q3) |> 
+  arrange(areaname, desc(muacres))
 glimpse(res3)
 
 write_csv(res3, paste0(local_dir, "/nrcs_county_soils_alt.csv"))
 write_rds(res3, paste0(local_dir, "/nrcs_county_soils_alt.rds"))
+
+crops_q <- "SELECT cyield.*, m.*, l.*
+            FROM cocropyld AS cyield
+            INNER JOIN component AS c on c.cokey = cyield.cokey
+            INNER JOIN mapunit m ON m.mukey = c.mukey
+            INNER JOIN legend l on m.lkey = l.lkey 
+            WHERE l.areasymbol LIKE 'OH%' AND cyield.cropname LIKE 'corn%'"
+crops <- SDA_query(crops_q)
+
+wot = SDA_query("SELECT DISTINCT cropname FROM cocropyld")
 
 
 # SELECT l.areaname, musym, muname, muacres, farmlndcl, compname,
